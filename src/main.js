fd0b297060af7319b694938d27229fe98f63ca50
ammon-m <106837909+ammon-m@users.noreply.github.com>
2024-03-21 13:06:44 -0700

    initial #2

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import * as Math3d from "./lib/Math3d.js";
+
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+const View = {
+    padding: 4,
+    width: 0,
+    height: 0
+}
+
+canvas.width = window.innerWidth - View.padding * 2
+canvas.height = window.innerHeight - View.padding * 2
+
+View.width = canvas.width
+View.height = canvas.height
+
+window.addEventListener("resize", event => {
+    canvas.width = window.innerWidth - View.padding * 2
+    canvas.height = window.innerHeight - View.padding * 2
+
+    View.width = canvas.width
+    View.height = canvas.height
+})
